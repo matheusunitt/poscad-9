3eb3449c9d7e7f2b337f5b4faa6e63dea8967f41
Jeffrey Matheus <matheusunitt@gmail.com>
2020-10-23 22:02:59 -0300

    Terminada a etapa 4

--- heranca/exibeFunc.php
+++ heranca/exibeFunc.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once "classes/Funcionario.php";
+require_once "classes/Gerente.php";
+require_once "classes/Programador.php";
+
 $nome = $_POST['nome'];
 $salario = $_POST['salario'];
 $obs = $_POST['obs'];
@@ -8,4 +12,12 @@ $tipo_funcionario = $_POST['tipo_funcionario'];
 echo "<p>Nome: {$nome}</p>";
 echo "<p>Salário: {$salario}</p>";
 echo "<p>Observações: {$obs}</p>";
-echo "<p>Tipo de funcionário: {$tipo_funcionario}</p>";
+echo "<p>Tipo de funcionário: Gerente</p>";
+
+if ($tipo_funcionario == 1) {
+    $funcionario = new Gerente($nome, $salario, $obs);
+} elseif ($tipo_funcionario == 1) {
+    $funcionario = new Programador($nome, $salario, $obs);
+} else {
+    echo "Tipo de funcionário inexistente. Tente novamente";
+}
